3e82fb2c5010afd373f7a3b7da089cc33e018fc3
Greg Scott <Greg.scott92@icloud.com>
2026-04-03 12:34:54 -0500

    create working va screen

---
+++ app/va.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function VAScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 28, fontWeight: "700" }}>
+        VA SCREEN WORKING
+      </Text>
+    </View>
+  );
+}
